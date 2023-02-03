5e452c484c97859864f83e4e2766313a9aadd3f4
Abdinajib <ynaj99@gmail.com>
2023-02-03 12:23:47 -0800

    deployed on github pages

---
+++ build/precache-manifest.13b65bb9c52c2e736f3bc3d5f7fdf044.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d08c0ece38965037c1d6",
+    "url": "/LetterLingo/static/css/main.2c3120d1.chunk.css"
+  },
+  {
+    "revision": "d08c0ece38965037c1d6",
+    "url": "/LetterLingo/static/js/main.d08c0ece.chunk.js"
+  },
+  {
+    "revision": "ab2d10c7890fe76b9da6",
+    "url": "/LetterLingo/static/js/1.ab2d10c7.chunk.js"
+  },
+  {
+    "revision": "82dfa91e80dfc3acdb0d",
+    "url": "/LetterLingo/static/js/runtime~main.82dfa91e.js"
+  },
+  {
+    "revision": "5cebf76d1168db2a62a5d2d0e001001a",
+    "url": "/LetterLingo/index.html"
+  }
+];
